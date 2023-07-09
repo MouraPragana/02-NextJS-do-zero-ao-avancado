c3e6821e9331e09eb124deb2964bffb1d2286380
Mateus Pragana <mateuspragana@gmail.com>
2023-07-09 17:35:51 -0300

    feat: finalizando aula - trabalhando com useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Nome } from "./components/Nome";
+import { useState } from "react";
 
 export function App() {
+  const [aluno, setAluno] = useState<string>("Sujeito Programador");
+
+  function handleChangeName(name: string) {
+    setAluno(name);
+  }
+
   return (
     <div>
       <h1>Componente App</h1>
-      <Nome aluno="Lucas" idade={30} />
+      <h2>Olá: {aluno}</h2>
+      <button onClick={() => handleChangeName("Lucas Oliveira")}>
+        Mudar Nome
+      </button>
       <br />
-      <Nome aluno="Jose Silva" idade={25} />
     </div>
   );
 }
