66743fda594235d1f646d6cb211ed051d1581d96
Mateus Pragana <mateuspragana@gmail.com>
2023-07-09 17:52:06 -0300

    feat: finalizando aula - Manipulando formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,72 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface Iuser {
+  nome: string;
+  email: string;
+  idade: string;
+}
 
 export function App() {
-  const [aluno, setAluno] = useState<string>("Sujeito Programador");
+  const [nome, setNome] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [idade, setIdade] = useState<string>("");
+  const [user, setUser] = useState({ nome: "", email: "", idade: "" } as Iuser);
 
-  function handleChangeName(name: string) {
-    setAluno(name);
+  function handleRegister(e: FormEvent) {
+    e.preventDefault();
+    alert("Usuario registrado com sucesso !");
+    setUser({ email, idade, nome });
   }
 
   return (
     <div>
-      <h1>Componente App</h1>
-      <h2>Olá: {aluno}</h2>
-      <button onClick={() => handleChangeName("Lucas Oliveira")}>
-        Mudar Nome
-      </button>
+      <form onSubmit={handleRegister}>
+        <h1>Cadastrando usuário</h1>
+        <label>Nome:</label>
+        <br />
+        <input
+          placeholder="Digite seu nome"
+          value={nome}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setNome(e.target.value)
+          }
+        />
+        <br />
+
+        <label>Email:</label>
+        <br />
+        <input
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+        <br />
+
+        <label>Idade:</label>
+        <br />
+        <input
+          placeholder="Digite sua idade"
+          value={idade}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setIdade(e.target.value)
+          }
+        />
+        <br />
+
+        <button type="submit">Registrar</button>
+      </form>
       <br />
+      <br />
+
+      <div>
+        <span>Bem vindo: {user.nome}</span>
+        <br />
+        <span>Idade: {user.idade}</span>
+        <br />
+        <span>Email: {user.email}</span>
+      </div>
     </div>
   );
 }
